c6b4cac67feccf76699263b7e4489d0a7e98dc91
adrian <adrianivan0010@gmail.com>
2022-10-11 16:59:20 +0300

    improved speed

--- spam.py
+++ spam.py
@@ -1,9 +1,12 @@
-import pyautogui, time
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.01
 time.sleep(5)
-f=open('beemovie.txt', 'r')
+f = open('beemovie.txt', 'r')
 for word in f.readlines():
     if not word.strip():
         continue
     if word:
-            pyautogui.typewrite(word)
-            pyautogui.press('enter')
+        pyautogui.typewrite(word)
+        pyautogui.press('enter')
